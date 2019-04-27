c4e7b2d46c5cd3d9c0b27b28203aefafcc5b62ee
Juntao Han <499445428@qq.com>
2019-04-27 20:17:42 +0800

    Add email service

---
+++ steam-core/src/main/java/org/steam/core/service/MailService.java
@@ -0,0 +1,47 @@
+package org.steam.core.service;
+
+/**
+ * 邮件服务
+ *
+ * @author mingshan
+ */
+public interface MailService {
+    /**
+     * 发送简单邮件
+     *
+     * @param to
+     * @param subject
+     * @param content
+     */
+    public void sendSimpleMail(String to, String subject, String content);
+
+    /**
+     * 发送html邮件
+     *
+     * @param to
+     * @param subject
+     * @param content
+     */
+    public void sendHtmlMail(String to, String subject, String content);
+
+    /**
+     * 发送带附件的邮件
+     *
+     * @param to
+     * @param subject
+     * @param content
+     * @param filePath
+     */
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath);
+
+    /**
+     * 发送带静态资源的邮件
+     *
+     * @param to
+     * @param subject
+     * @param content
+     * @param rscPath
+     * @param rscId
+     */
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId);
+}
---
+++ steam-core/src/main/java/org/steam/core/service/impl/MailServiceImpl.java
@@ -0,0 +1,110 @@
+package org.steam.core.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.steam.core.service.MailService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
+
+/**
+ * @author mingshan
+ */
+@Service
+@Slf4j
+public class MailServiceImpl implements MailService {
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${mail.fromMail.address}")
+    private String from;
+
+    @Override
+    public void sendSimpleMail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        try {
+            mailSender.send(message);
+            log.info("邮件发送成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.info("邮件发送失败" );
+        }
+    }
+
+    @Override
+    public void sendHtmlMail(String to, String subject, String content) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            //true表示需要创建一个multipart message
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+            mailSender.send(message);
+            log.info("html邮件发送成功");
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            log.info("html邮件发送失败" );
+
+        }
+
+    }
+
+    @Override
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            FileSystemResource file = new FileSystemResource(new File(filePath));
+            String fileName = filePath.substring(filePath.lastIndexOf(File.separator));
+            helper.addAttachment(fileName, file);
+
+            mailSender.send(message);
+            log.info("带附件的邮件已经发送。");
+        } catch (MessagingException e) {
+            log.error("发送带附件的邮件时发生异常！", e);
+        }
+    }
+
+    @Override
+    public void sendInlineResourceMail(String to, String subject, String content, String rscPath, String rscId) {
+        MimeMessage message = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content, true);
+
+            FileSystemResource res = new FileSystemResource(new File(rscPath));
+            helper.addInline(rscId, res);
+
+            mailSender.send(message);
+            log.info("嵌入静态资源的邮件已经发送。");
+        } catch (MessagingException e) {
+            log.error("发送嵌入静态资源的邮件时发生异常！", e);
+        }
+    }
+}
