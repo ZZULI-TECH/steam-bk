560f64c543734b1750d0d245a2608cbcf9b29dcc
Juntao Han <499445428@qq.com>
2019-05-02 15:45:42 +0800

    Update EmailTest.java

--- steam-core/src/test/java/org/steam/core/service/EmailTest.java
+++ steam-core/src/test/java/org/steam/core/service/EmailTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.steam.common.exception.ServiceException;
-import org.steam.core.service.MailService;
 
 /**
  * @author mingshan
