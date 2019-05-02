b8c6f50225164d65bf51347d318c2973b1cff42b
Juntao Han <499445428@qq.com>
2019-05-02 18:11:04 +0800

    Fix: ServiceException -> ServerException

--- steam-core/src/main/java/org/steam/core/service/impl/UserServiceImpl.java
+++ steam-core/src/main/java/org/steam/core/service/impl/UserServiceImpl.java
@@ -24,7 +24,6 @@ public class UserServiceImpl extends ServiceImpl<UserMapper,User> implements Use
         }
 
         VersionUtil.checkVersion(version, user);
-        user.setDeleted(1);
-        this.baseMapper.updateById(user);
+        this.baseMapper.deleteById(id);
     }
 }
