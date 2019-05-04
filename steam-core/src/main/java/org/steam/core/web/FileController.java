4de60c5503a12e387d4726f206393c4f5f7425b8
Juntao Han <499445428@qq.com>
2019-05-04 22:35:17 +0800

    Remove useless import

--- steam-core/src/main/java/org/steam/core/web/FileController.java
+++ steam-core/src/main/java/org/steam/core/web/FileController.java
@@ -9,8 +9,6 @@ import com.qiniu.storage.UploadManager;
 import com.qiniu.storage.model.DefaultPutRet;
 import com.qiniu.util.Auth;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
