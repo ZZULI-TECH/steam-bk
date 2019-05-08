95f2b548b873b011fd5e6a8efc379993bfec242a
liyufeng <liyufenglyff@163.com>
2019-05-08 11:20:05 +0800

    fix: long2String

--- steam-core/src/main/java/org/steam/core/json/config/SpringMvcConfig.java
+++ steam-core/src/main/java/org/steam/core/json/config/SpringMvcConfig.java
@@ -1,7 +1,12 @@
 package org.steam.core.json.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -19,6 +24,23 @@ public class SpringMvcConfig implements WebMvcConfigurer {
     @Bean
     public JsonReturnHandler JsonReturnHandler(){
         return new JsonReturnHandler();
+    }
+
+    /**
+     * @description long2String
+     * @date 2019/5/8 11:16
+     * @param converters
+     * @return void
+     */
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        SimpleModule simpleModule = new SimpleModule();
+        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
+        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
+        objectMapper.registerModule(simpleModule);
+        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
+        converters.add(jackson2HttpMessageConverter);
     }
 
     @Override
