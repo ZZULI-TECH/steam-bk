806410589334f4e0d4c54262ddb9451e2da2fa76
GitBiao <18838986375@163.com>
2019-05-12 14:26:01 +0800

    支付接口添加

--- steam-core/src/main/java/org/steam/core/web/PayController.java
+++ steam-core/src/main/java/org/steam/core/web/PayController.java
@@ -1,5 +1,8 @@
 package org.steam.core.web;
 
+import com.alipay.api.AlipayApiException;
+import com.alipay.api.internal.util.AlipaySignature;
+import com.jpay.alipay.AliPayApi;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +12,26 @@ import org.steam.common.annotation.Authorization;
 import org.steam.common.exception.ServerException;
 import org.steam.common.exception.ServiceException;
 import org.steam.common.model.ResultModel;
+import org.steam.core.model.entity.AliPayBean;
 import org.steam.core.model.entity.Token;
 import org.steam.core.model.entity.User;
+import org.steam.core.model.vo.PayPageVO;
 import org.steam.core.model.vo.PayVo;
 import org.steam.core.service.IPayService;
 import org.steam.core.util.TokenUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 @Api(value = "pay")
 @RestController
 @RequestMapping("/api/pay")
 public class PayController {
 
+    @Autowired
+    private AliPayBean aliPayBean;
     @Autowired
     private IPayService payService;
 
@@ -35,6 +47,52 @@ public class PayController {
             throw new ServerException(model, HttpStatus.INTERNAL_SERVER_ERROR);
         }
         return ResultModel.ok();
+    }
+
+    /**
+     * 这个接口用GET会跳转到支付宝页面
+     * @param response
+     * @param userId
+     * @param orderId
+     */
+    @ApiOperation(value="到支付宝页面支付订单", httpMethod="GET")
+    @Authorization
+    @RequestMapping("/pay/{userId}/{orderId}")
+    public void pay(HttpServletResponse response, @PathVariable long userId, @PathVariable long orderId){
+        PayPageVO payPage = payService.payOrder(orderId, userId);
+        try {
+            AliPayApi.tradePage(response, payPage.getModel(), payPage.getNotifyUrl(), payPage.getNotifyUrl());
+        } catch (AlipayApiException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @ApiOperation(value="支付订单", httpMethod="POST")
+    @RequestMapping("/notify")
+    public String payNotify(HttpServletRequest request){
+        try {
+            // 获取支付宝反馈信息
+            Map<String, String> params = AliPayApi.toMap(request);
+            for (Map.Entry<String, String> entry : params.entrySet()) {
+                System.out.println(entry.getKey() + " = " + entry.getValue());
+            }
+            boolean verify_result = AlipaySignature.rsaCheckV1(params, aliPayBean.getPublicKey(), "UTF-8", "RSA2");
+            if (verify_result) {// 验证成功
+                try {
+                    payService.payCallBack(params);
+                } catch (ServiceException e) {
+                    return "failure";
+                }
+                return "success";
+            } else {
+                return "failure";
+            }
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+            return "failure";
+        }
+
     }
 
 }
