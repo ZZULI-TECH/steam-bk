fe91916a4150d37d5087b042dca4d847ccedec83
GitBiao <18838986375@163.com>
2019-05-14 23:52:30 +0800

    mybatis 死活映射不上

--- steam-core/src/main/java/org/steam/core/repository/GameLibraryMapper.java
+++ steam-core/src/main/java/org/steam/core/repository/GameLibraryMapper.java
@@ -2,6 +2,7 @@ package org.steam.core.repository;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.steam.core.model.entity.GameLibrary;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.steam.core.model.vo.GameLibraryVO;
@@ -16,6 +17,14 @@ import org.steam.core.model.vo.GameLibraryVO;
  */
 public interface GameLibraryMapper extends BaseMapper<GameLibrary> {
 
+    @Select("SELECT\n" +
+            "            gl.id,gl.uid,gl.game_id,gl.gmt_create,gl.gmt_modified,gl.version,\n" +
+            "            g.name,g.type,g.english_name,g.click_count,g.sales,g.price,g.cover,g.keywords,g.remark,g.content,g.download_url\n" +
+            "        FROM\n" +
+            "            t_game_library gl\n" +
+            "            LEFT JOIN t_game g on gl.game_id = g.id\n" +
+            "        WHERE\n" +
+            "            gl.uid = #{uid} AND gl.deleted = 0")
     Page<GameLibraryVO> selectGameLibraryPage(Page<GameLibraryVO> page, @Param("uid")Long uid);
 
 }
